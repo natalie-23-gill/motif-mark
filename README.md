bc16a4a86bd659670ad51b57b0ca6b55c750af80
Natalie Elphick <nelphick@uoregon.edu>
2022-03-04 10:19:29 -0800

    Finish readme and put test files and output in a directory

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# motif-mark
-Script to create figures of motif markers in set of sequences from a fasta file
+## motif-mark | A command line utility to visualize protein binding motifs along with exon and introns    
+
+*How to use:*   
+
+` conda create --name motif-mark`    
+` conda activate motif-mark`   
+` conda install -c conda-forge pycairo`   
+` conda install -c conda-forge seaborn` 
+
+
+
+*Versions:*     
+`pycairo 1.20.1`   
+`seaborn-0.11.2`   
+`python  3.10.2`   
+
+### How to create figures   
+
+`> python3 motif-mark-oop.py -f [Fasta file] -m [Motif file] [Optional arguments]`   
+
+### Example output : 
+
+![Example Output](test_files/Figure_1.png)  
+
+
+### Additional Options :
+
+ Light mode output:   
+`python3 motif-mark-oop.py -f [Fasta file] -m [Motif file] -l`  
+Scale the nucleotides by a factor x:   
+`python3 motif-mark-oop.py -f [Fasta file] -m [Motif file] -s x`
--- motif-mark-oop.py
+++ motif-mark-oop.py
@@ -4,21 +4,60 @@ import argparse
 import math
 import cairo
 import re
-import numpy as np
 import seaborn as sns
 
 # Get args
 parser = argparse.ArgumentParser()
-parser.add_argument("-f","--filename",help="Input fasta file",required=True)
-parser.add_argument("-m","--motif_fn",help="Input motifs file, up to 7 motifs",required=True)
+parser.add_argument("-f","--filename",help="Input fasta file, ouput image will have same name and location.",required=True)
+parser.add_argument("-m","--motif_fn",help="Input motifs file, up to 7 motifs.",required=True)
 parser.add_argument("-l", "--lightmode", help="switch output to lightmode figure", action="store_true")
 parser.add_argument("-s", "--size", nargs="?",type=int,const=1,default=2,help="scale factor to lengthen each nucleotide, default is 1")
 args = parser.parse_args()
 
+#########    Global Variables    #########
+
+if not args.lightmode:
+    # Get colorblind friendly palette
+    color_pal = sns.color_palette("colorblind")
+    # Remove Gray
+    color_pal.remove((0.5803921568627451, 0.5803921568627451, 0.5803921568627451))
+    line_col = (.95,.95,.95)
+else:
+    # light mode colors
+    color_pal = sns.color_palette("colorblind")
+    # Remove Gray
+    color_pal.remove((0.5803921568627451, 0.5803921568627451, 0.5803921568627451))
+    line_col = (.4,.4,.4)
+
+def draw_legend(c_key,x_pos,y_pos):
+    x = x_pos+10
+    y = y_pos+50
+    context.select_font_face("Lato", cairo.FONT_SLANT_NORMAL,cairo.FONT_WEIGHT_NORMAL)
+    context.set_font_size(18)
+    context.set_source_rgb(line_col[0],line_col[1],line_col[2]) 
+    context.move_to(x,y-30)
+    context.show_text("Legend:")
+    for i in c_key:
+
+        context.select_font_face("Lato", cairo.FONT_SLANT_NORMAL,cairo.FONT_WEIGHT_NORMAL)
+        context.set_font_size(18)
+        context.set_source_rgb(c_key[i][0],c_key[i][1],c_key[i][2])
+        context.move_to(x,y)
+        context.show_text(i)
+        x+=100
+# width for exons
+exon_width = 50
+
+# introns line width
+intron_width = 2
+
+pad_header = 50
+left_margin = 10
 
 ##### Define file parsing functions ######
    
 def parse_fasta(file):
+    '''Parses fasta file and returns a list of sequences and a list of fasta headers'''
     with open(file, "r") as fh:
         
         header_list = []
@@ -91,7 +130,7 @@ class motif_mark:
     
   ## Methods ##
     def draw_seq(self):
-
+        '''Draws the sequence'''
         # left margin
         x=self.origin[0] +5
         
@@ -108,17 +147,14 @@ class motif_mark:
         context.move_to(x,y)
         context.show_text(self.header)
 
-        for i in self.ex_in:
-            #print(i.span())
-            #print(i.group())
-            
+        for i in self.ex_in:            
             if i.group().isupper(): # check if exon or intron
                 
                 context.set_line_width(exon_width+args.size+4)
             else:
                 
                 context.set_line_width(intron_width)
-                
+            # Draw seq, with adjusted line width
             context.move_to(args.size*i.span()[0]+x,y+pad_header) 
             context.line_to(args.size*i.span()[1]+x,y+pad_header)
             context.set_source_rgb(line_col[0],line_col[1],line_col[2]) 
@@ -144,33 +180,30 @@ class motif_mark:
 
                 match_dict[m].append(i.span()) 
         
-        jitter =0
+        jitter =-1
         jit_dict = {}
         for i in match_dict:
-            jit_dict[i]=jitter # add .5 in increments and assign to each motif
+            
             jitter+=.5
+            jit_dict[i]=jitter # add .5 in increments and assign to each motif
             
             for k in match_dict[i]:
 
                 # height for start point
                 rect_start_y=(y+pad_header)-exon_width/2 +jit_dict[i]
-                
+                # draw a rectangle
                 context.move_to(args.size*k[0]+x,rect_start_y)
                 context.line_to(args.size*k[0]+x+len(i)*args.size,rect_start_y) 
                 context.line_to(args.size*k[0]+x+len(i)*args.size,rect_start_y+exon_width)
                 context.line_to(args.size*k[0]+x,rect_start_y+exon_width)
                 context.line_to(args.size*k[0]+x,rect_start_y)
-
                 context.close_path()
-
+                # fill rectangle
                 context.set_source_rgba(col_key[i][0],col_key[i][1],col_key[i][2],.25) 
                 context.fill_preserve()
-
-                context.set_source_rgba(col_key[i][0],col_key[i][1],col_key[i][2],.7)  
-                
+                # outline
+                context.set_source_rgba(col_key[i][0],col_key[i][1],col_key[i][2],.85)  
                 context.set_line_width(args.size)
-                
-                
                 context.stroke()   
 
 
@@ -185,76 +218,30 @@ seqs = file_lists[0]
 # Parse motif file
 motifs = parse_motifs(args.motif_fn)
 
+#########    Create Objects     ##########
 
-
-
-
-#########    Global Variables    #########
-
-if not args.lightmode:
-    # Get colorblind friendly palette
-    color_pal = sns.color_palette("colorblind")
-    # Remove Gray
-    color_pal.remove((0.5803921568627451, 0.5803921568627451, 0.5803921568627451))
-    line_col = (.95,.95,.95)
-else:
-    # light mode colors
-    color_pal = sns.color_palette("colorblind")
-    # Remove Gray
-    color_pal.remove((0.5803921568627451, 0.5803921568627451, 0.5803921568627451))
-    line_col = (.4,.4,.4)
-
-def draw_legend(c_key,x_pos,y_pos):
-    x = x_pos+10
-    y = y_pos+50
-    context.select_font_face("Lato", cairo.FONT_SLANT_NORMAL,cairo.FONT_WEIGHT_NORMAL)
-    context.set_font_size(18)
-    context.set_source_rgb(line_col[0],line_col[1],line_col[2]) 
-    context.move_to(x,y-30)
-    context.show_text("Legend:")
-    for i in c_key:
-
-        context.select_font_face("Lato", cairo.FONT_SLANT_NORMAL,cairo.FONT_WEIGHT_NORMAL)
-        context.set_font_size(18)
-        context.set_source_rgb(c_key[i][0],c_key[i][1],c_key[i][2])
-        context.move_to(x,y)
-        context.show_text(i)
-        x+=100
-
-
-col_key ={} #track colors for each motif
+# dictionary to track colors for each motif
+col_key ={} 
 col_count = 0
 for i in motifs:
     col_key[i]=color_pal[col_count]
     col_count+=1
 
-
-# width for exons
-exon_width = 50
-
-# introns line width
-intron_width = 2
-
-pad_header = 50
-left_margin = 10
-
-#########    Create Objects     ##########
-
 ob_dict = {}
-
 start_y = 20 # starting y pos
+
 for i in range(len(headers)):
+
     ob_dict[i]=motif_mark((left_margin,start_y),seqs[i],motifs,headers[i])
     i+=1
-    start_y+=120 # increment by 100
-
-
+    start_y+=125 # increment by 100 to move the y origin for each figure
+    # *could add an option here to incerase the spacing between plots* 
 
 #########    Draw Background     ##########
 
 
 # width = max width of each figure 
-# height = number of motif_marks * width of each + padding
+# height = number of motif_marks * width of each + padding for legend
 WIDTH, HEIGHT = len(max(seqs))*args.size+300*args.size, len(seqs)*120+150
 # create the coordinates to display the graphic
 surface = cairo.ImageSurface (cairo.FORMAT_ARGB32, WIDTH, HEIGHT)
@@ -271,16 +258,14 @@ else:
 context.rectangle(0, 0, WIDTH, HEIGHT)
 context.fill()
 
-####### Draw using the objects ######
-
-
+####### Draw figures using the objects ######
 for i in ob_dict:
     ob_dict[i].draw_seq()
     ob_dict[i].draw_motifs()
 
+# Draw legend
 draw_legend(col_key,left_margin,start_y)
 
 
 #write out drawing
-
 surface.write_to_png (args.filename.split(".")[0]+".png") # Output to PNG
